fb7fee734d76029c31bc86aef247a96079217236
maxim <maximus77t@gmail.com>
2023-01-26 15:46:43 +0300

    Task3

--- Program.cs
+++ Program.cs
@@ -2,20 +2,20 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-// Console.WriteLine("Ввведите число");
-// int N = Convert.ToInt32(Console.ReadLine());
-// int Sum(int num)
-// {
-//     if (num == 1)
-//     {
-//     return 1;
-//     }
-//     Console.Write(num);
-//     return Sum(num - 1);   
-// }
-// int nat= Sum(N);
+Console.WriteLine("Ввведите число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int Sum(int num)
+{
+    if (num == 1)
+    {
+    return 1;
+    }
+    Console.Write(num);
+    return Sum(num - 1);   
+}
+int n2= Sum(n1);
 
-// Console.WriteLine((nat));
+Console.WriteLine((n2));
 
 
 
@@ -23,16 +23,35 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-// Console.WriteLine("Ввведите M");
-// int M = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Ввведите N");
-// int N = Convert.ToInt32(Console.ReadLine());
-// int sum = 0;
-// for (int i = M; i <= N; i++)
-// {
-//     sum = sum + i;
-// }
-// Console.WriteLine(sum);
+Console.WriteLine("Ввведите первое число");
+int m1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввведите второе число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = m1; i <= n1; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine(sum);
 
 
 
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Ввведите первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввведите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+int Ack(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (m == 0)
+        return Ack(n - 1, 1);
+    else
+        return Ack(n - 1, Ack(n, m - 1));
+
+}
+Console.WriteLine(Ack(m, n));
