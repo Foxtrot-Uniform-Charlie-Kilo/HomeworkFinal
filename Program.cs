1c2b31184030836830850bf13fd4b1481b020dcd
maxim <maximus77t@gmail.com>
2023-01-26 15:40:56 +0300

    Task2

--- Program.cs
+++ Program.cs
@@ -2,18 +2,37 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+// Console.WriteLine("Ввведите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int Sum(int num)
+// {
+//     if (num == 1)
+//     {
+//     return 1;
+//     }
+//     Console.Write(num);
+//     return Sum(num - 1);   
+// }
+// int nat= Sum(N);
 
-Console.WriteLine("Ввведите число");
-int N = Convert.ToInt32(Console.ReadLine());
-int Sum(int num)
-{
-    if (num == 1)
-    {
-    return 1;
-    }
-    Console.Write(num);
-    return Sum(num - 1);   
-}
-int nat= Sum(N);
+// Console.WriteLine((nat));
 
-Console.WriteLine((nat));
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.WriteLine("Ввведите M");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Ввведите N");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// for (int i = M; i <= N; i++)
+// {
+//     sum = sum + i;
+// }
+// Console.WriteLine(sum);
+
+
+
