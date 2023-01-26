5f6df46114539c80b2d5cf9ef5edf591ca4a681b
Foxtrot-Uniform-Charlie-Kilo <122045887+Foxtrot-Uniform-Charlie-Kilo@users.noreply.github.com>
2023-01-26 15:53:08 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -44,14 +44,14 @@ Console.WriteLine("Ввведите первое число");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Ввведите второе число");
 int n = Convert.ToInt32(Console.ReadLine());
-int Ack(int n, int m)
+int AM(int n, int m)
 {
     if (n == 0)
         return m + 1;
     else if (m == 0)
-        return Ack(n - 1, 1);
+        return AM(n - 1, 1);
     else
-        return Ack(n - 1, Ack(n, m - 1));
+        return AM(n - 1, AM(n, m - 1));
 
 }
-Console.WriteLine(Ack(m, n));
+Console.WriteLine(AM(m, n));
